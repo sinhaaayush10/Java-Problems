bc2bf7f1385cdc448a08ff79cf29750a029d88ac
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-08-10 21:16:21 +0530

    func(i-1,n)
    To Run :-
    javac S,java
    java S
    5
    
    Output:- 
    5 4 3 2 1

---
+++ Q4.Print N to 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class S {
+static void func(int i, int n ){
+if(i<1) return;
+
+System.out.print(i+ " ");
+func(i-1,n);
+}
+
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+func(n,n);
+}
+}
